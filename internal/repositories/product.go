9f447fe3c3c9837d9803ddde5ec82dbefd482acd
hamillka <ilyasov060@gmail.com>
2025-04-12 16:09:05 +0300

    [+] finished data access layer

--- internal/repositories/product.go
+++ internal/repositories/product.go
@@ -1,1 +1,107 @@
 package repositories
+
+import (
+	"time"
+
+	"github.com/hamillka/avitoTechSpring25/internal/handlers/dto"
+	"github.com/hamillka/avitoTechSpring25/internal/models"
+	"github.com/jmoiron/sqlx"
+)
+
+type ProductRepository struct {
+	db *sqlx.DB
+}
+
+const (
+	addProduct               = "INSERT INTO products (product_type, reception_id) VALUES ($1, $2) RETURNING id, date_time, product_type, reception_id"
+	getProductsByReceptionId = "SELECT * FROM products WHERE reception_id = $1 AND date_time BETWEEN $2 AND $3"
+	getLastProduct           = "SELECT * FROM products WHERE reception_id = $1 ORDER BY date_time DESC LIMIT 1"
+	deleteProduct            = "DELETE FROM products WHERE id = $1"
+)
+
+func NewProductRepository(db *sqlx.DB) *ProductRepository {
+	return &ProductRepository{
+		db: db,
+	}
+}
+
+func (pr *ProductRepository) AddProduct(productType, receptionId string) (models.Product, error) {
+	var product models.Product
+
+	err := pr.db.QueryRow(addProduct, productType, receptionId).
+		Scan(
+			&product.Id,
+			&product.DateTime,
+			&product.Type,
+			&product.ReceptionId,
+		)
+	if err != nil {
+		return models.Product{}, dto.ErrDBInsert
+	}
+
+	return product, nil
+}
+
+func (pr *ProductRepository) GetLastProduct(recId string) (models.Product, error) {
+	var product models.Product
+	err := pr.db.QueryRow(getLastProduct, recId).
+		Scan(
+			&product.Id,
+			&product.DateTime,
+			&product.Type,
+			&product.ReceptionId,
+		)
+	if err != nil {
+		return models.Product{}, dto.ErrNoProductsInReception
+	}
+
+	return product, nil
+}
+
+func (pr *ProductRepository) DeleteProduct(prodId string) error {
+	_, err := pr.db.Exec(deleteProduct, prodId)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (pr *ProductRepository) GetProductsByReceptionId(recId string, startDate, endDate *time.Time) ([]models.Product, error) {
+	start := time.Time{}
+	end := time.Now()
+
+	if startDate != nil {
+		start = *startDate
+	}
+
+	if endDate != nil {
+		end = *endDate
+	}
+
+	var products []models.Product
+
+	rows, err := pr.db.Query(getProductsByReceptionId, recId, start, end)
+	if err != nil {
+		return products, nil
+	}
+	if err := rows.Err(); err != nil {
+		return products, nil
+	}
+
+	for rows.Next() {
+		product := models.Product{}
+		if err := rows.Scan(
+			&product.Id,
+			&product.DateTime,
+			&product.Type,
+			&product.ReceptionId,
+		); err != nil {
+			return nil, dto.ErrDBRead
+		}
+		products = append(products, product)
+	}
+	defer rows.Close()
+
+	return products, nil
+}
--- internal/repositories/user.go
+++ internal/repositories/user.go
@@ -1,1 +1,61 @@
 package repositories
+
+import (
+	"database/sql"
+	"errors"
+
+	"github.com/hamillka/avitoTechSpring25/internal/handlers/dto"
+	"github.com/hamillka/avitoTechSpring25/internal/models"
+	"github.com/jmoiron/sqlx"
+)
+
+type UserRepository struct {
+	db *sqlx.DB
+}
+
+const (
+	createUser     = "INSERT INTO users (email, password_hash, role) VALUES ($1, $2, $3) RETURNING id, email, password_hash, role"
+	getUserByEmail = "SELECT id, email, password_hash, role FROM users WHERE email = $1"
+)
+
+func NewUserRepository(db *sqlx.DB) *UserRepository {
+	return &UserRepository{
+		db: db,
+	}
+}
+
+func (ur *UserRepository) UserRegister(email, password, role string) (models.User, error) {
+	var user models.User
+	err := ur.db.QueryRow(createUser, email, password, role).
+		Scan(
+			&user.Id,
+			&user.Email,
+			&user.Password,
+			&user.Role,
+		)
+	if err != nil {
+		return models.User{}, dto.ErrDBInsert
+	}
+
+	return user, nil
+}
+
+func (ur *UserRepository) UserLogin(email, password string) (models.User, error) {
+	var user models.User
+
+	err := ur.db.QueryRow(getUserByEmail, email).
+		Scan(
+			&user.Id,
+			&user.Email,
+			&user.Password,
+			&user.Role,
+		)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return models.User{}, dto.ErrInvalidCredentials
+		}
+		return models.User{}, dto.ErrDBRead
+	}
+
+	return user, nil
+}
