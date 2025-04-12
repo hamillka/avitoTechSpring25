ec79143ad62b8b8f68e213f648d2d6fec6c38f85
hamillka <ilyasov060@gmail.com>
2025-04-12 16:16:55 +0300

    [+] testDB init sql script for integration tests

---
+++ sql-scripts/1-init-test.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE pvz_service_test;
+
+\connect "pvz_service_test";
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    role TEXT NOT NULL CHECK (role IN ('employee', 'moderator'))
+);
+
+CREATE TABLE pvzs (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    registration_date TIMESTAMPTZ DEFAULT NOW(),
+    city TEXT NOT NULL CHECK (city IN ('Москва', 'Санкт-Петербург', 'Казань'))
+);
+
+CREATE TABLE receptions (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    date_time TIMESTAMPTZ DEFAULT NOW(),
+    pvz_id UUID NOT NULL REFERENCES pvzs(id) ON DELETE CASCADE,
+    status TEXT NOT NULL DEFAULT 'in_progress' CHECK (status IN ('in_progress', 'close'))
+);
+
+CREATE TABLE products (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    date_time TIMESTAMPTZ DEFAULT NOW(),
+    product_type TEXT NOT NULL CHECK (product_type IN ('электроника', 'одежда', 'обувь')),
+    reception_id UUID NOT NULL REFERENCES receptions(id) ON DELETE CASCADE
+);
