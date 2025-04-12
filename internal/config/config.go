43e37a9f416a2385541e0cb843c8b671af199598
hamillka <ilyasov060@gmail.com>
2025-04-12 16:11:05 +0300

    [+] added config package

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,25 @@
 package config
+
+import (
+	"github.com/hamillka/avitoTechSpring25/internal/db"
+	"github.com/hamillka/avitoTechSpring25/internal/logger"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	DB      db.DatabaseConfig `envconfig:"DB"`
+	Port    string            `envconfig:"PORT"`
+	Timeout int64             `envconfig:"TIMEOUT"`
+	Log     logger.LogConfig  `envconfig:"LOG"`
+}
+
+func New() (*Config, error) {
+	var config Config
+
+	err := envconfig.Process("", &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
